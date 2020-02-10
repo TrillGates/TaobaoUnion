5d2ae5a8fa021faff15d5f609805e713b716c416
849077547 <849077547@qq.com>
2020-02-10 18:21:11 +0800

    init project.

---
+++ app/src/main/java/com/sunofbeaches/taobaounion/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sunofbeaches.taobaounion.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sunofbeaches.taobaounion.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

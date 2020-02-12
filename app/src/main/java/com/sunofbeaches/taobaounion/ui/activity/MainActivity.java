4a1a6d424c6e50a49e517c34d3e0d497458ac148
849077547 <849077547@qq.com>
2020-02-12 13:48:29 +0800

    completed page switch.

--- app/src/main/java/com/sunofbeaches/taobaounion/ui/activity/MainActivity.java
+++ app/src/main/java/com/sunofbeaches/taobaounion/ui/activity/MainActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.sunofbeaches.taobaounion.R;
+import com.sunofbeaches.taobaounion.base.BaseFragment;
 import com.sunofbeaches.taobaounion.ui.fragment.HomeFragment;
+import com.sunofbeaches.taobaounion.ui.fragment.RedPacketFragment;
+import com.sunofbeaches.taobaounion.ui.fragment.SearchFragment;
+import com.sunofbeaches.taobaounion.ui.fragment.SelectedFragment;
 import com.sunofbeaches.taobaounion.utils.LogUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,17 +19,30 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
     @BindView(R.id.main_navigation_bar)
     public BottomNavigationView mNavigationView;
+    private HomeFragment mHomeFragment;
+    private RedPacketFragment mRedPacketFragment;
+    private SelectedFragment mSelectedFragment;
+    private SearchFragment mSearchFragment;
+    private FragmentManager mFm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        initView();
+        initFragments();
         initListener();
+    }
+
+    private void initFragments() {
+        mHomeFragment = new HomeFragment();
+        mRedPacketFragment = new RedPacketFragment();
+        mSelectedFragment = new SelectedFragment();
+        mSearchFragment = new SearchFragment();
+        mFm = getSupportFragmentManager();
+        switchFragment(mHomeFragment);
     }
 
     private void initListener() {
@@ -33,23 +50,25 @@ public class MainActivity extends AppCompatActivity {
             // Log.d(TAG,"title -- > " + item.getTitle() + " id -- > " + item.getItemId());
             if(item.getItemId() == R.id.home) {
                 LogUtils.d(MainActivity.class,"切换到首页");
-                //TODO
+                switchFragment(mHomeFragment);
             } else if(item.getItemId() == R.id.selected) {
                 LogUtils.i(MainActivity.class,"切换到精选");
+                switchFragment(mSelectedFragment);
             } else if(item.getItemId() == R.id.red_packet) {
                 LogUtils.w(MainActivity.class,"切换到特惠");
+                switchFragment(mRedPacketFragment);
             } else if(item.getItemId() == R.id.search) {
                 LogUtils.e(MainActivity.class,"切换到搜索");
+                switchFragment(mSearchFragment);
             }
             return true;
         });
     }
 
-    private void initView() {
-        HomeFragment homeFragment = new HomeFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        transaction.add(R.id.main_page_container,homeFragment);
-        transaction.commit();
+    private void switchFragment(BaseFragment targetFragment) {
+        FragmentTransaction fragmentTransaction = mFm.beginTransaction();
+        fragmentTransaction.replace(R.id.main_page_container,targetFragment);
+        fragmentTransaction.commit();
     }
+
 }
--- app/src/main/java/com/sunofbeaches/taobaounion/ui/fragment/SelectedFragment.java
+++ app/src/main/java/com/sunofbeaches/taobaounion/ui/fragment/SelectedFragment.java
@@ -1,21 +1,12 @@
 package com.sunofbeaches.taobaounion.ui.fragment;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.sunofbeaches.taobaounion.R;
 import com.sunofbeaches.taobaounion.base.BaseFragment;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 public class SelectedFragment extends BaseFragment {
 
     @Override
     protected int getRootViewResId() {
-        return R.layout.fragment_home;
+        return R.layout.fragment_selected;
     }
 }

f3f123535357afb2ef93018d356e3d2d56eb77aa
TrillGates <wx_f93f2baa60e84b219009ff090d32fbf0@git.code.tencent.com>
2020-03-25 22:32:25 +0800

    completed flow text layout measure.

--- app/src/main/java/com/sunofbeaches/taobaounion/ui/custom/TextFlowLayout.java
+++ app/src/main/java/com/sunofbeaches/taobaounion/ui/custom/TextFlowLayout.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -18,6 +19,7 @@ public class TextFlowLayout extends ViewGroup {
     public static final float DEFAULT_SPACE = 10;
     private float mItemHorizontalSpace = DEFAULT_SPACE;
     private float mItemVerticalSpace = DEFAULT_SPACE;
+    private int mSelfWidth;
 
 
     public float getItemHorizontalSpace() {
@@ -70,11 +72,72 @@ public class TextFlowLayout extends ViewGroup {
         }
     }
 
+    //这个是描述单行
+    private List<View> line = null;
+    //这个是描述所有的行
+    private List<List<View>> lines = new ArrayList<>();
+
     @Override
     protected void onMeasure(int widthMeasureSpec,int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec,heightMeasureSpec);
+        mSelfWidth = MeasureSpec.getSize(widthMeasureSpec) - getPaddingLeft() - getPaddingRight();
+        LogUtils.d(this,"mSelfWidth == > " + mSelfWidth);
         //测量
         LogUtils.d(this,"onMeasure -- > " + getChildCount());
+        //测量孩子
+        int childCount = getChildCount();
+        for(int i = 0; i < childCount; i++) {
+            View itemView = getChildAt(i);
+            //测量前
+            LogUtils.d(this,"before height -- > " + itemView.getMeasuredHeight());
+            measureChild(itemView,widthMeasureSpec,heightMeasureSpec);
+            //测量后
+            LogUtils.d(this,"after height -- > " + itemView.getMeasuredHeight());
+            if(line == null) {
+                //说明当前行为空，可以添加进来
+                createNewLine(itemView);
+            } else {
+                //判断是否可以再继续添加
+                if(canBeAdd(itemView,line)) {
+                    //可以添加，添加去
+                    line.add(itemView);
+                } else {
+                    //新创建一行
+                    createNewLine(itemView);
+                }
+            }
+        }
+        int selfHeight = (int) (lines.size() * getChildAt(0).getMeasuredHeight() + mItemVerticalSpace * (lines.size() + 1) + 0.5f);
+        //测量自己
+        setMeasuredDimension(mSelfWidth,selfHeight);
+    }
+
+    private void createNewLine(View itemView) {
+        line = new ArrayList<>();
+        line.add(itemView);
+        lines.add(line);
+    }
+
+    /**
+     * 判断当前行是否可以再继续添加新数据
+     *
+     * @param itemView
+     * @param line
+     */
+    private boolean canBeAdd(View itemView,List<View> line) {
+        //所有已经添加的子view宽度相加+(line.size()+1)*mItemHorizontalSpace + itemView.getMeasuredWidth()
+        //条件：如果小于/等于当前控件的宽度，则可以添加，否则不能添加
+        int totalWith = itemView.getMeasuredWidth();
+        for(View view : line) {
+            //叠加所有已经添加控件的宽度
+            totalWith += view.getMeasuredWidth();
+        }
+        //水平间距的宽度
+        totalWith += mItemHorizontalSpace * (line.size() + 1);
+        LogUtils.d(this,"totalWith -- > " + totalWith);
+        LogUtils.d(this,"mSelfWidth -- > " + mSelfWidth);
+        //如果小于/等于当前控件的宽度，则可以添加，否则不能添加
+        return totalWith <= mSelfWidth;
     }
 
 

9f16121b051954a513581ad78155ff21f0a2e257
TrillGates <1447585309@qq.com>
2020-05-28 19:39:34 +0800

    Update Constants.java
    update base url.

--- app/src/main/java/com/sunofbeaches/taobaounion/utils/Constants.java
+++ app/src/main/java/com/sunofbeaches/taobaounion/utils/Constants.java
@@ -2,7 +2,7 @@ package com.sunofbeaches.taobaounion.utils;
 
 public class Constants {
     //base url
-    public static final String BASE_URL = "https://www.sunofbeach.net/shop/api/";
+    public static final String BASE_URL = "https://api.sunofbeach.net/shop/";
     //home pager fragment bundle key.
     public static final String KEY_HOME_PAGER_TITLE = "key_home_pager_title";
     public static final String KEY_HOME_PAGER_MATERIAL_ID = "key_home_pager_material_id";
